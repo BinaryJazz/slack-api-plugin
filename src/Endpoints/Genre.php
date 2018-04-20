765fb59c60886b8f6dcceea46e8281ca0824e897
Gary Kovar <gkovar@gmail.com>
2018-04-19 22:30:51 -0400

    adds genrenator endpoint

---
+++ src/Endpoints/Genre.php
@@ -0,0 +1,25 @@
+<?php
+
+namespace BinaryJazz\Slack\Endpoints;
+
+
+class Genre extends Base {
+
+	const ENDPOINT = '/genre';
+
+	public function register() {
+		register_rest_route( self::PATH, self::ENDPOINT, [
+			'methods'  => 'POST',
+			'callback' => [ $this, 'genre' ],
+		] );
+	}
+
+	public function endpoint() {
+		return self::ENDPOINT;
+	}
+
+	public function genre() {
+
+	}
+
+}
--- src/Service_Providers/Endpoints_Provider.php
+++ src/Service_Providers/Endpoints_Provider.php
@@ -3,6 +3,7 @@
 namespace BinaryJazz\Slack\Service_Providers;
 
 
+use BinaryJazz\Slack\Endpoints\Genre;
 use BinaryJazz\Slack\Endpoints\OAuth;
 use Pimple\Container;
 use Pimple\ServiceProviderInterface;
@@ -10,14 +11,20 @@ use Pimple\ServiceProviderInterface;
 class Endpoints_Provider implements ServiceProviderInterface {
 
 	const ENDPOINTS_OAUTH = 'endpoints.oauth';
+	const ENDPOINTS_GENRE = 'endpoints.genre';
 
 	public function register( Container $container ) {
 		$container[ self::ENDPOINTS_OAUTH ] = function () use ( $container ) {
 			return new OAuth( $container[ Slack_Provider::POST_MESSAGE ], $container[ Slack_Provider::REDIRECT_URI ] );
 		};
 
+		$container[ self::ENDPOINTS_GENRE ] = function () use ( $container ) {
+			return new Genre( $container[ Slack_Provider::POST_MESSAGE ] );
+		};
+
 		add_action( 'rest_api_init', function () use ( $container ) {
 			$container[ self::ENDPOINTS_OAUTH ]->register();
+			$container[ self::ENDPOINTS_GENRE ]->register();
 		} );
 
 	}
